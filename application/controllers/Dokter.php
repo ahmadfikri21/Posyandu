3ab7d43135730c821a728ecd5eb87e23509ad12a
nengahdharmaa <61053807+nengahdharmaa@users.noreply.github.com>
2020-04-20 01:15:49 +0700

    cssnya udah di rubah

--- application/controllers/Dokter.php
+++ application/controllers/Dokter.php
@@ -2,17 +2,47 @@
 defined('BASEPATH') or exit('No direct script access allowed');
 
 class Dokter extends CI_Controller
-{
+{	
+    function __construct(){
+    parent::__construct();
+    $this->load->model('dokter_model');
+    $this->load->library('session');
+  
+    }
     public function index()
     {
         $this->load->view("templates/dokter/headerHome");
         $this->load->view("Dokter/login");
         $this->load->view("templates/dokter/footerHome");
     }
+<<<<<<< HEAD
+=======
+
+    public function Login()
+    {
+        $data['username'] = $this->input->post('username');
+		$data['password'] = $this->input->post('password');
+        
+		if($this->dokter_model->login_dokter($data)){
+			$this->session->set_userdata('username', $this->input->post('username'));
+            $this->session->set_userdata('password', $this->input->post('password'));
+            redirect('Dokter/homepage');
+           
+		}else{
+         
+            $data['error_message'] = "Invalid Username or Password";
+            echo $data['error_message'];
+			//$this->load->view('login',$data);
+		}
+    }
+>>>>>>> b130be914f078b1e4dc351269fd2b0ea2e3c2407
     public function homepage()
     {
+        $username = $this->session->userdata('username');
+    $data['datadokter'] = $this->dokter_model->get_profile($username);
+        $data = $data['datadokter'][0];
         $this->load->view("templates/dokter/headerHome");
-        $this->load->view("Dokter/Homepage");
+        $this->load->view("Dokter/homepage",$data);
         $this->load->view("templates/dokter/footerHome");
     }
 
--- application/views/Dokter/login.php
+++ application/views/Dokter/login.php
@@ -1,16 +1,25 @@
 <section class="container-fluid section-color-dk">
+<<<<<<< HEAD
     <div class="h1-login-dk">Selamat Datang Dokter</div>
     <form >
+=======
+    <h1 class="h1-login-dk">Selamat Datang Dokter</h1>
+    <?php echo form_open('Dokter/Login') ?>
+>>>>>>> b130be914f078b1e4dc351269fd2b0ea2e3c2407
   <div class="form-group ">
-    <input type="text" class="form-control input-login-dk" id="exampleInputEmail1"  placeholder="Username">
+    <input type="text" class="form-control input-login-dk" id="exampleInputEmail1"  placeholder="Username" name="username">
   </div>
   <div class="form-group">
-    <input type="password" class="form-control input-login-dk" id="exampleInputPassword1" placeholder="Password">
+    <input type="password" class="form-control input-login-dk" id="exampleInputPassword1" placeholder="Password" name="password">
   </div>
   <div class="form-check">
     <input type="checkbox" class="form-check-input" id="exampleCheck1">
     <label class="form-check-label" for="exampleCheck1">Remember me</label>
   </div>
+<<<<<<< HEAD
   <button type="submit" class="btn btn-primary btn-login-dk">Login</button>
+=======
+  <button type="submit" class="btn btn-primary btn-login-dk" href="">Submit</button>
+>>>>>>> b130be914f078b1e4dc351269fd2b0ea2e3c2407
 </form> 
 </section>
