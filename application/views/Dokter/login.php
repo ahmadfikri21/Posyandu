6794c609fdb59cef6abfd954a4eb998c326c9ebf
Maulidito <maulidito@gmail.com>
2020-04-19 20:07:04 +0700

    mengedit login dokter

--- application/views/Dokter/login.php
+++ application/views/Dokter/login.php
@@ -1,39 +1,16 @@
-<div class="container-fluid p-0 container-dk">
-    <div class="container">
-        <div class="row justify-content-center">
-            <div class="col-lg-5">
-                <div class="card o-hidden border-0 shadow-lg my-5">
-                    <div class="card-body p-0">
-                        <div class="row">
-                            <div class="col-lg">
-                                <div class="p-5">
-                                    <div class="text-center">
-                                        <h1 class="h4 text-gray-900 mb-4">Login Page</h1>
-                                    </div>
-                                    <form class="user">
-                                        <div class="form-group">
-                                            <input type="text" class="form-control form-control-user" id="ussername" name="ussername" placeholder="Ussername">
-                                        </div>
-                                        <div class="form-group">
-                                            <input type="password" class="form-control form-control-user" id="password" name="password" placeholder="Password">
-                                        </div>
-                                        <button type="submit" class="btn btn-primary btn-user ">
-                                            Login
-                                        </button>
-                                    </form>
-                                    <hr>
-
-                                    <div class="text-center">
-                                        <a class="small" href="<?php echo base_url(""); ?>homepage/registrasi">Belum punya akun? daftar disini!</a>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-
-            </div>
-
-        </div>
-    </div>
-</div>
+<section class="container-fluid section-color-dk">
+    <h1 class="h1-login-dk">Selamat Datang Dokter</h1>
+    <form >
+  <div class="form-group ">
+    <input type="text" class="form-control input-login-dk" id="exampleInputEmail1"  placeholder="Username">
+  </div>
+  <div class="form-group">
+    <input type="password" class="form-control input-login-dk" id="exampleInputPassword1" placeholder="Password">
+  </div>
+  <div class="form-check">
+    <input type="checkbox" class="form-check-input" id="exampleCheck1">
+    <label class="form-check-label" for="exampleCheck1">Remember me</label>
+  </div>
+  <button type="submit" class="btn btn-primary btn-login-dk">Submit</button>
+</form> 
+</section>
