7a2df8e118e1b5e7df12bdd915ceb65d43a2d15e
nengahdharmaa <61053807+nengahdharmaa@users.noreply.github.com>
2020-04-21 10:16:05 +0700

    diubah lagi

--- application/views/Dokter/homepage.php
+++ application/views/Dokter/homepage.php
@@ -5,7 +5,7 @@
             <br>
             <p class="text-center"><?= $nama?></p>
             <p class="text-center">Center</p>
-            <a href="<?=site_url('Dokter/Edit')?>">asd</a>
+            <a href="<?=site_url('Dokter/Edit')?>">edit profile</a>
             <br>    
             <button type="button" class="btn btn-secondary btn-lg btn-home-dk">Input Laporan Kesehatan <br>Pasien </button>
             <button type="button" class="btn btn-secondary btn-lg btn-home-dk">Isi Jadwal Praktek  </button>
