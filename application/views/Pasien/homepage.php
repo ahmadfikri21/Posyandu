d168a471bdc71bc30abc69fdf50d6d9cc467df57
Ahmad Fikri <fikriahmad705@gmail.com>
2020-04-22 21:55:23 +0700

    update laporan kesehatan dan riwayat

--- application/views/Pasien/homepage.php
+++ application/views/Pasien/homepage.php
@@ -5,6 +5,7 @@
     <section class="container-isi">
         <h2><center>Informasi Terbaru</center></h2>
         <?php foreach($informasi as $info): ?>
+        <small> Tanggal Dibuat <Strong><?= $info['tgl_dibuat'] ?></Strong></small>
         <p><?php echo $info['isi'] ?></p>
         <?php endforeach; ?>
     </section>
--- application/views/Pasien/laporanKesehatan.php
+++ application/views/Pasien/laporanKesehatan.php
@@ -1,21 +1,23 @@
-<div class="container">
-    <div class="heading-lap-panel">
-        <h1>Hasil Pemeriksaan <?= $laporan['nm_pasien'] ?></h1>
-        <div class="keterangan">
-            <div class="kolom">
-                <small>Tanggal Diperiksa <strong><?= $laporan['tanggal'] ?></strong></small><br>
-                <small>Kategori <strong><?= $laporan['kategori'] ?></strong></small><br>
-                <small>Diperiksa Oleh <strong>Dr. <?= $laporan['nm_dokter'] ?></strong></small>
-            </div>
-            <div class="kolom">
-                <input type="image" src="<?php echo base_url() ?>assets/css/img/icon-pdf.png">
+<div class="body-lap-kesehatan">
+    <div class="container">
+        <div class="heading-lap-panel">
+            <h1>Hasil Pemeriksaan <?= $laporan['nm_pasien'] ?></h1>
+            <div class="keterangan">
+                <div class="kolom">
+                    <small>Tanggal Diperiksa <strong><?= $laporan['tanggal'] ?></strong></small><br>
+                    <small>Kategori <strong><?= $laporan['kategori'] ?></strong></small><br>
+                    <small>Diperiksa Oleh <strong>Dr. <?= $laporan['nm_dokter'] ?></strong></small>
+                </div>
+                <div class="kolom">
+                    <input type="image" src="<?php echo base_url() ?>assets/css/img/icon-pdf.png">
+                </div>
             </div>
         </div>
-    </div>
-    <div class="hasil-lap-panel">
-        <div class="scroll-list-riwayat">
-            <p><?= $laporan['hasil_Pemeriksaan'] ?></p>
+        <div class="hasil-lap-panel">
+            <div class="scroll-list-riwayat">
+                <p><?= $laporan['hasil_Pemeriksaan'] ?></p>
+            </div>
+            <a href="<?php echo base_url() ?>Pasien/riwayat" class="btn btn-info"><--Kembali</a>
         </div>
-        <a href="<?php echo base_url() ?>Pasien/riwayat" class="btn btn-info"><--Kembali</a>
     </div>
 </div>
