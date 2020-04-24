c2689a70ef62a1b35886784b6ba1f6e1fe7dd81f
nengahdharmaa <61053807+nengahdharmaa@users.noreply.github.com>
2020-04-24 10:18:18 +0700

    css input

--- application/views/Dokter/homepage.php
+++ application/views/Dokter/homepage.php
@@ -4,9 +4,9 @@
             <center>
             <img src="<?= base_url('/assets/css/img/Logo_Dokter.png')?>" class="img-fluid" alt="Responsive image">
             <br>
-            <p class="text-center"><?= $nama?></p>
-            <p class="text-center">Center</p>
-            <a class="eidt-profile" href="<?=site_url('Dokter/Edit')?>">edit profile</a>
+            <p class="text-center">Dokter <?= $nama?></p>
+            <p class="text-center"><?= $username?></p>
+            <a class="edit-profile" href="<?=site_url('Dokter/Edit')?>">edit profile</a>
             <br>    
              <a class="btn btn-secondary btn-lg btn-home-dk" href="<?php echo site_url('Dokter/daftarpasien');?>">
             Input Laporan Kesehatan <br>Pasien </a>
