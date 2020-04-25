6366129125109bbf51dba85617ebdd7a2896440d
nengahdharmaa <61053807+nengahdharmaa@users.noreply.github.com>
2020-04-25 08:30:19 +0700

    Update homepage.php

--- application/views/Dokter/homepage.php
+++ application/views/Dokter/homepage.php
@@ -6,12 +6,17 @@
             <br>
             <p class="text-center">Dokter <?= $nama?></p>
             <p class="text-center"><?= $username?></p>
-            <a class="edit-profile" href="<?=site_url('Dokter/Edit')?>">edit profile</a>
+            <a class="edit-profile" href="<?=site_url('Dokter/editdokter')?>">edit profile</a>
             <br>    
              <a class="btn btn-secondary btn-lg btn-home-dk" href="<?php echo site_url('Dokter/daftarpasien');?>">
             Input Laporan Kesehatan <br>Pasien </a>
+<<<<<<< HEAD
             <a class="btn btn-secondary btn-lg btn-home-dk" href="<?php echo site_url('Dokter/viewJadwalPraktek');?>"> 
             Jadwal Praktek  </a>
+=======
+            <a class="btn btn-secondary btn-lg btn-home-dk" href="<?php echo site_url('Dokter/logout');?>"> 
+            Logout </a>
+>>>>>>> d31ec53572d4b0dc1373b98258dd44225aaa2e31
             </center>
         </section>
 </section>
