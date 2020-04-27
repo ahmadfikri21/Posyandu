426db953a55887e6bf8d45def6d386ca1debbd6e
Maulidito <maulidito@gmail.com>
2020-04-27 17:23:20 +0700

    keloladokter blom jadi, mau nambahin add jadwal praktek di kelola dokter tapi males :(

--- application/views/Pengelola/kelolaDokter.php
+++ application/views/Pengelola/kelolaDokter.php
@@ -2,30 +2,36 @@
     <div class="panel-kelola-dokter">
         <h1>Kelola Informasi</h1>
         <?php echo form_open('pengelola/searchDK') ?>
-        <!-- <div class="form-group form-inline">
+       <div class="form-group form-inline">
             <label>cari</label>
             <input type="text" name="cari" class="form-control" placeholder="cari">
             <input type="submit" value="Cari" class="btn btn-info">
-        </div> -->
+        </div> 
         <?php form_close(); ?>
-        <a href="<?php echo base_url() ?>Pengelola/tambahDokter" class="btn btn-info tambah-dk">Tambah Dokter</a>
+        <a href="<?php echo base_url() ?>Pengelola/tambahDokter" class="btn btn-info tambah-dk">Tambah Dokter</a> 
         <table class="table table-striped">
             <thead>
                 <tr class="head-tb-dk">
                     <th>No.</th>
-                    <th>tanggal dibuat</th>
-                    <th>Isi Informasi</th>
+                    <th>ID Dokter</th>
+                    <th>Nama</th>
+                    <th>Username</th>
+                    <th>no Telp</th>
                 </tr>
             </thead>
             <tbody>
                 <?php $i = 1 ?>
-                <?php foreach($informasi as $key): ?>
+                <?php foreach($isiDokter as $key): ?>
                 <tr>
                     <td><?= $i++; ?></td>
-                    <td><?= $key['tgl_dibuat'] ?></td>
-                    <td><?= $key['isi'] ?></td>
-                    <!-- <td><a class="btn btn-info btn-small" href="<?php echo site_url('/Pengelola/editDokter/' . $dokter['id_dokter']); ?>">Edit</a>
-                        <a class="btn btn-danger btn-small" href="<?php echo site_url('/Pengelola/hapusDokter/' . $dokter['id_dokter']); ?>">Hapus</a></td> -->
+                    <td><?= $key['id_dokter'] ?></td>
+                    <td><?= $key['nama'] ?></td>
+                    <td><?= $key['username'] ?></td>
+                    <td><?= $key['no_telp'] ?></td>
+                     <td><a class="btn btn-info btn-small" href="<?php echo site_url('/Pengelola/editDokter/' . $key['id_dokter']); ?>">Edit</a>
+                        <a class="btn btn-danger btn-small" href="<?php echo site_url('/Pengelola/hapusDokter/' . $key['id_dokter']); ?>">Hapus</a>
+                        <a class="btn btn btn-info btn-small" href="<?php echo site_url( $key['id_dokter']); ?>">Jadwal Praktek</a>
+                        </td> 
                 </tr>
                 <?php endforeach; ?>
             </tbody>
